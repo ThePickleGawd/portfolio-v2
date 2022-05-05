00d202185256f2a2439bdaa017ad698518f050ff
Dylan Lu <dylanelu@gmail.com>
2022-05-05 08:57:36 -0700

    merge

--- components/Home/WiggleText.tsx
+++ components/Home/WiggleText.tsx
@@ -13,11 +13,12 @@ export const WiggleText = ({ text }: { text: string }) => {
 const WiggleLetter = ({ letter }: { letter: string }) => {
   const [wiggle, setWiggle] = useState(false);
 
-  if (letter == " ") return <div className={`text-8xl`}>&nbsp;</div>;
+  if (letter == " ")
+    return <div className={`text-8xl select-none`}>&nbsp;</div>;
 
   return (
     <div
-      className={`text-white text-8xl font-semibold hover:text-sky-400 ${
+      className={`text-white text-8xl font-semibold hover:text-sky-400 select-none ${
         wiggle && "animate-wiggle"
       }`}
       onMouseOver={() => setWiggle(true)}
